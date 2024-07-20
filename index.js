fca0d93fda2c7719dcfd470a657d3432437b3202
sopan1000 <145831690+sopan1000@users.noreply.github.com>
2024-07-20 22:51:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+import hero from "/src/public/assets/images/hero.jpeg";
+
+export const heroImg = hero;
+
+import hmv from "/src/public/assets/videos/hero.mp4";
+import smallmv from "/src/public/assets/videos/smallHero.mp4";
+import highlightFirstmv from "/src/public/assets/videos/highlight-first.mp4";
+import highlightSectmv from "/src/public/assets/videos/hightlight-third.mp4";
+import highlightThirdmv from "/src/public/assets/videos/hightlight-sec.mp4";
+import highlightFourthmv from "/src/public/assets/videos/hightlight-fourth.mp4";
+import exploremv from "/src/public/assets/videos/explore.mp4";
+import framemv from "/src/public/assets/videos/frame.mp4";
+
+import apple from "/src/public/assets/images/apple.svg";
+import search from "/src/public/assets/images/search.svg";
+import bag from "/src/public/assets/images/bag.svg";
+import watch from "/src/public/assets/images/watch.svg";
+import right from "/src/public/assets/images/right.svg";
+import replay from "/src/public/assets/images/replay.svg";
+import play from "/src/public/assets/images/play.svg";
+import pause from "/src/public/assets/images/pause.svg";
+
+import yellow from "/src/public/assets/images/yellow.jpg";
+import blue from "/src/public/assets/images/blue.jpg";
+import white from "/src/public/assets/images/white.jpg";
+import black from "/src/public/assets/images/black.jpg";
+import explore1 from "/src/public/assets/images/explore1.jpg";
+import explore2 from "/src/public/assets/images/explore2.jpg";
+import chip from "/src/public/assets/images/chip.jpeg";
+import frame from "/src/public/assets/images/frame.png";
+
+export const heroVideo = hmv;
+export const smallHeroVideo = smallmv;
+export const highlightFirstVideo = highlightFirstmv;
+export const highlightSecondVideo = highlightSectmv;
+export const highlightThirdVideo = highlightThirdmv;
+export const highlightFourthVideo = highlightFourthmv;
+export const exploreVideo = exploremv;
+export const frameVideo = framemv;
+
+export const appleImg = apple;
+export const searchImg = search;
+export const bagImg = bag;
+export const watchImg = watch;
+export const rightImg = right;
+export const replayImg = replay;
+export const playImg = play;
+export const pauseImg = pause;
+
+export const yellowImg = yellow;
+export const blueImg = blue;
+export const whiteImg = white;
+export const blackImg = black;
+export const explore1Img = explore1;
+export const explore2Img = explore2;
+export const chipImg = chip;
+export const frameImg = frame;
